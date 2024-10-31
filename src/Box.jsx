5027e20cc1bbe4b00d86f3d7c9971a258492de91
Ahmed <a.hennawy97@hotmail.com>
2024-10-31 20:03:26 +0300

    Added app neccessary files

---
+++ src/Box.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "Box.css";
+
+function Box() {}
+
+export default Box;
---
+++ src/BoxList.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import Box from "./Box";
+import "./BoxList.css";
+
+function BoxList() {
+  return <></>;
+}
+
+export default BoxList;
